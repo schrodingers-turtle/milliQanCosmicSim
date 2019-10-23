7f1095a51b3cf7858d85455271db9a325270a2aa
Mukesh Ghimire <mg5213@pd334.physics.nyu.edu>
2019-10-23 17:47:11 -0400

    added E_min mode per annulus. I goes down eventually as we go farther from the center, as expected

--- csim.py
+++ csim.py
@@ -1,57 +1,54 @@
 import random
-import numpy as np
+import scipy as sp
 import pylab as pl
 
 #initialize cosmic variables
 I_0   	= 88.0
 n     	= 3
 E_0   	= 4.29
-E_min	= 10. #cutoff value of Energy
 E_max 	= 4000.
 epinv 	=  1/854.
 Rod   	= 174.
-Norm	= (n-1)*(E_0 + E_min)**(n-1)
 
 #initialize milliQan variables
-depth 	= 60	
-srfrad	= 1000	#surface radius for rmax
-chmbrad = 2	#chamber radius
+depth 	= 60.	
+srfrad	= 1000.		#surface radius for rmax
+chmbrad = 2.		#chamber radius
 
 #initialize simulation variables
 rmin 	= 0
-rmax 	= 5
+rmax 	= 100
 rstep	= 1
-npart	= 10000
+npart	= 1000
 
-def Dfun(theta):
-	return np.sqrt(Rod**2*np.cos(theta)**2 + 2*Rod + 1) - Rod*np.cos(theta)
+def Dfun(theta):	#from ref[1]
+	return sp.sqrt(Rod**2*sp.cos(theta)**2 + 2*Rod + 1) - Rod*sp.cos(theta)
 
-def Ifun(E,theta):
-	return I_0*Norm*(E_0 + E)**(-n)*(1 + E*epinv)**(-1)*Dfun(theta)**(-(n-1))
+def Ifun(E,theta):	#also from ref[1]
+	return I_0*(E_0 + E)**(-n)*(1 + E*epinv)**(-1)*Dfun(theta)**(-(n-1))
 
-def test(I):	#for testing the sampled muons by energy distribution at theta = 0
-	N = 101
-	incr = (np.log10(4000)-1)/N
+def test(I,E_min):	#for testing the sampled muons by energy distribution at theta = 0
+	N = 100
+	incr = sp.log10(E_max/E_min)/N
 	analytx = []
 	analyty = []
-	for i in range(0,N):
-		xx	= 10**(i*incr + 1)
+	for i in range(0,N+1):
+		xx	= E_min*10**(i*incr)
 		analytx.append(xx)
 		analyty.append(I(xx))
 
 	Imax = max(analyty)
 
 	j		= 0
-	Npts		= 1001
 
 	samplx = []
 	samply = []
 	failx = []
 	faily = []
-	for i in range(1,Npts):
-		x = (np.log10(4000)-1)*random.random()
+	for i in range(1,npart*10):
+		x = sp.log10(E_max/E_min)*random.random()
 		y = Imax*random.random()
-		xp = 10**(x + 1)
+		xp = E_min*10**x
 		yp = y
 		if y <= I(xp):
 			j += 1
@@ -69,43 +66,45 @@ def test(I):	#for testing the sampled muons by energy distribution at theta = 0
 	ax.set_xscale('log')
 	pl.show()
 
-def genmuons(I):
+def genmuons(I, E_min):
 	Imax = I(E_min)
 	Elist = []
-	for i in range(0,npart):
-		x = (np.log10(4000)-1)*random.random()
+	for i in range(0,npart*10):
+		x = sp.log10(E_max/E_min)*random.random()
 		y = Imax*random.random()
-		xp = 10**(x + 1)
+		xp = E_min*10**x
 		if y <= I(xp):
 			Elist.append([xp])
 	return Elist
 
 def annul(r_a, r_b):
 	r_mid 		= (r_a+r_b)/2.
-	theta 		= np.arctan(r_mid/60.)
-	phisub 		= 1.*(2*np.pi/360)
-	theta_a		= np.arctan(r_a/60.)
-	theta_b		= 2*theta - theta_a
-	sldangl 	= phisub*(np.cos(theta_a) - np.cos(theta_b))
-	area		= (r_b**2 - r_a**2)*phisub/2.
+	E_min		= sp.sqrt(r_mid**2+60**2)-2	#min energy is 1 GeV per meter of rock
+	theta 		= sp.arctan(r_mid/60.)
+	
+	area		= (r_b**2 - r_a**2)*sp.pi
 
-	thetap_a	= np.arctan((r_mid - 2.)/60.)
-	thetap_b	= theta + np.arctan(2/np.sqrt(60**2 + r_mid**2))
-	phip		= np.arctan(2/r_mid)
+	thetap_a	= sp.arctan((r_mid - chmbrad)/depth)
+	thetap_b	= theta + sp.arcsin(chmbrad/sp.sqrt(depth**2 + r_mid**2))
+	phip		= sp.pi
+	if r_mid > chmbrad:
+		phip	= sp.arcsin(chmbrad/r_mid)
+
+	sldangl 	= 2*phip*(sp.cos(thetap_a) - sp.cos(thetap_b))
 
 	def I(E):
 		return area*sldangl*Ifun(E,theta)
 	
-	test(I)		#produces a rate vs log momentum plot
+#	test(I,E_min)		#produces a rate vs log momentum plot
 	
-	mlist = genmuons(I)
+	mlist = genmuons(I,E_min)
 	
 	for i in range(0,len(mlist)):
-		phi = random.uniform(0,2*np.pi)
-		mlist[i].append(r_mid)
+		phi = random.uniform(0,2*sp.pi)
+		mlist[i].append(random.uniform(r_a,r_b))
 		mlist[i].append(phi)
 		mlist[i].append(random.uniform(thetap_a,thetap_b))
-		mlist[i].append(random.uniform(-phip,phip)+phi)
+		mlist[i].append((random.uniform(-phip,phip)+phi)%(2*sp.pi))
 	
 	return mlist
 
@@ -114,10 +113,14 @@ def main():
 
 	mlist = []
 
-	while len(mlist) < npart:
+	while len(mlist) < npart/10:
 		for i in range(1,Nsteps+1):
-			mlist += annul(rmin+(i-1)*rstep,rmin + i*rstep)	
+			mlist += annul(rmin+(i-1)*rstep,rmin + i*rstep)
 	print(mlist)
 
 if __name__ == '__main__':
 	main()
+
+#references:
+#[1] arXiv:1606.06907v3
+
